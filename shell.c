fac3c6db152814a64f3a24e9b03a9c315169b3aa
rsancho64 <rsancho@iesrioarba.es>
2023-10-26 13:13:28 +0200

    first commit

---
+++ shell.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    int num;
+    char nombre[10];
+
+    // un entero
+    // printf("Introduce un número: ");
+    // scanf("%i", &num);
+    // printf("Has tecleado el número %i\n", num);
+
+    // un string, pero un solo token (palabra)
+    // printf("introduce un string: ");
+    // scanf("%s", nombre);
+    // printf("Has tecleado: %s\n", nombre);
+
+    // puts-gets
+    // char cadena[50];
+    // puts("Escriba un texto:");
+    // gets(cadena);
+    // puts("El texto escrito es:");
+    // puts(cadena);
+
+    #define BUFFERSIZE 50
+
+    char comando[BUFFERSIZE];
+    char *p;
+    const char s[2] = " "; // UNICO SEPARADOR: ESPACIO EN BLANCO   
+    char *token0;    
+
+    for (;;)
+    {
+        printf("? ");
+        p = fgets(comando, BUFFERSIZE, stdin);
+        if (p)
+        {
+            // puts("echo");
+            // puts(comando); // to do puts(eval(comando))
+
+            /* first token */
+            token0 = strtok(comando, s);
+            printf("tok: %s\n", token0 );            
+
+            // TODO: imprimir los demas tokens
+
+        }
+        // else
+        //     puts("comando vacio");
+    }
+}
