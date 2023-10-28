932e5217e27cc2985af12103aee91d15de62ca7e
rsancho64 <rsancho@iesrioarba.es>
2023-10-28 13:18:22 +0200

    .

--- en.python/shell.py
+++ en.python/shell.py
@@ -1,52 +1,63 @@
 #! /usr/bin/python3
 # from tokenize import tokenize, untokenize, NUMBER, STRING, NAME, OP
-# REPL:
-#     READ   read()
-#     EVAL   eval()
-#     PRINT  print()
-#     LOOP
-#
-#     while (True) : print(eval(read()))
+# a REPL is a { Read -read()-; Eval -eval()-; Print -print()- } Loop
+# while (True) : print(eval(read()))
 #
 
 def myPrint(str):
     """print output string from expresion evaluation"""
     if str == "":
-        print(str, end='')
+        return  # pass # print(str, end='')
+    print(str)
+
 
 def myHelp():
-    print("lo siento, no hay ayuda por ahora")
+    return "lo siento, no hay ayuda por ahora"
+
 
 def myEval(e):
 
     if e == "":
-        return e # None
+        return e  # None
 
     if e in ["exit", "bye", "adios"]:
         print("bye")
         exit(0)
 
     if e in ["help", "/h", "?"]:
-        myHelp()
-        return None
+        return myHelp()
 
-    return eval(e)
+    # tokenizing e(xpr) ...
+    # https://blog.gitnux.com/code/python-tokenizing-strings-in-list-of-strings
+    # :/
+    # import nltk
+    # nltk.download('punkt')
+    # tokens = nltk.word_tokenize(e)
+    # return tokens
+
+    # https://www.tutorialspoint.com/5-simple-ways-to-perform-tokenization-in-python
+    # :/
+    # goo: parse python expression in tokens ...
+    # - [] try pyparsing
+    # - [] try TDParser    
+
+    # return eval(e)
 
 if __name__ == "__main__":
     """wrapper REPL en python"""
 
-    while True:        
+    while True:
         # read
-        algo = input("dime algo: ")
-        # print(algo)
+        # print(e)
+        e = input("dime algo: ")
 
         # eval ; print
-        myPrint(myEval(algo))
+        myPrint(myEval(e))
 
-#def help():
+# def help():
 #    return "no hay contenido de ayuda todavia"
 
-#def myEval(e):
+# def myEval(e):
 
 #    e = e.lower() ## canonize(exp)
 
@@ -60,21 +71,16 @@ if __name__ == "__main__":
 #    return eval(e) # https://www.mygreatlearning.com/blog/eval-in-python
 #    # return f"{exp}: desconocido"
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 
 #    """REPL: bucle read,eval,print o print(eval(read()))"""
 #    while True:
-    
+
         # R
 #        inp = input("e? ")  # print("e? ", end = '') ";" in = input()
 
         # E
 #        out = myEval(inp)
-        
+
         # P
 #        print(out)
-
-
-
-        
-
